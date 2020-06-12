4b9a11d5b60f41a05ffa2e00e85b2b5126c8a92b
sagar-wal <sagaragarwal612@gmail.com>
2020-06-13 00:51:20 +0530

    add whitenoise

--- Sphinx/settings.py
+++ Sphinx/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'quiz/static')
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
