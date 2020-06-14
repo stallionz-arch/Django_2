d721e34bc9f583cbc7630ca19919a78a7ba73f48
stallionz-arch <65930656+stallionz-arch@users.noreply.github.com>
2020-06-14 18:45:03 +0530

    Add files via upload
    heroku

--- Django-Sphinx-Project/Sphinx/settings.py
+++ Django-Sphinx-Project/Sphinx/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'social_django',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,7 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
